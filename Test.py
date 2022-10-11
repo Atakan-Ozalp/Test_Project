8d80f992646d32292f3889a39382c87d882919d0
Atakan-Ozalp <atakan.ozalp1@gmail.com>
2022-10-11 11:34:47 +0300

    Test Commit

---
+++ Test.py
@@ -0,0 +1,24 @@
+#🚨 Don't change the code below👇
+import random
+
+test_seed =int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+names_string =input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+#🚨 Don't change the code above👆
+
+#Write your code below this line👇
+num_items =len(names)
+random_choice = random.randint(0, num_items -1)
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + " is going to buy the meal today.")
+
+
+#Much simpler and easier way to do this is
+
+person_who_will_pay = random.choice(names)
+print(person_who_will_pay + " is going to buy the meal today!")
+
+
