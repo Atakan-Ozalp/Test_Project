567e75ea9391326438cfbdc82f94c9bee92d1106
Atakan-Ozalp <atakan.ozalp1@gmail.com>
2022-10-12 08:11:11 +0300

    1231

--- Test.py
+++ Test.py
@@ -1,24 +1,53 @@
-#🚨 Don't change the code below👇
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
 import random
 
-test_seed =int(input("Create a seed number: "))
-random.seed(test_seed)
+game_images = [rock, paper, scissors]
+Human = int(input("What do you choose? \n 0 for Rock, \n 1 for Paper, \n 2 for Scissors?\n"))
+if Human >= 3 or Human < 0:
+  print("You typed an invalid number, you lose!")
+else:
+  print("You chose")
+  print(game_images[Human])
+  Computer = random.randint(0, 2)
+  print(f"Computer chose")
+  print(game_images[Computer])
 
-# Split string method
-names_string =input("Give me everybody's names, separated by a comma. ")
-names = names_string.split(", ")
-#🚨 Don't change the code above👆
-
-#Write your code below this line👇
-num_items =len(names)
-random_choice = random.randint(0, num_items -1)
-person_who_will_pay = names[random_choice]
-print(person_who_will_pay + " is going to buy the meal today.")
-
-
-#Much simpler and easier way to do this is. Random garbage
-
-person_who_will_pay = random.choice(names)
-print(person_who_will_pay + " is going to buy the meal today!")
-
-
+  if Human == 0 and Computer == 2:
+    print("You win")
+  elif Computer == 0 and Human == 2:
+    print("You lose")
+  elif Computer > Human:
+    print("You lose")
+  elif Human > Computer:
+    print("You win")
+  elif Computer == Human:
+    print("It's a draw")
+  else:
+    print("You typed an invalid number, you lose")
